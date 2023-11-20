dc2d8d9e3742fc4ee3a2abfe5dc663e12ee1245b
Seva Makhinia <vsevolod.makhinia@oracle.com>
2023-11-20 17:59:06 +0200

    Samples of variables, operators, floating points

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,42 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Java vendor: " + System.getProperty("java.vendor"));
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Java home: " + System.getProperty("java.home"));
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        String myString = "StringLiteral";  // String literal
+//        int myInt = 100500;                 // Integer literal
+//        Integer myInteger = myInt;          // int -> Integer automagically
+////        Float myFloat = myInteger;        // Integer -> Float does not happen
+//
+////        System.out.printf("My string is %d, my int is %d, and my integer is %d\n\r",
+////                myString, myInt, myInteger);
+////        System.out.printf("My string is %d, my int is %f, and my integer is %f\n\r",
+////                myString, myInt, myInteger);
+//        System.out.printf("My string is %s, my int is %d, and my integer is %d%s",
+//                myString, myInt, myInteger, System.lineSeparator());
+////        myInt = 2147483623; // Integer.MAX_VALUE - 25;
+////        var myNextInt = myInt;
+////        for (int c = 0; c < 50; c++) {
+////            System.out.println(++myInt + " :: " + myNextInt++);
+////        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+//// 		System.out.println(true > false);
+//// 		System.out.println(true + 8);
+//        System.out.println(true + "8");
+//        System.out.println("8" + true);
+
+////        var myFloat = 123456789;
+////        var myFloat = 123456789f;
+//        float myFloat = 123456789;
+//        float anotherFloat = 1;
+//        System.out.printf("%9f :: %.9f\n\r",
+//                myFloat, myFloat - anotherFloat, System.lineSeparator());
+//        for (int c = 0; c < 100; c++) {
+//            myFloat -= 1;
+//        }
+//        System.out.printf("%9f :: %.9f\n\r",
+//                myFloat, myFloat - anotherFloat, System.lineSeparator());
     }
 }
